c195b0dd6ab81fcd997e671442ea7e259382bd84
Piyush Verma <github@piyushverma.net>
2015-02-17 16:22:11 +0530

    Move stuff to db/oplog instead of main.

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/aderjaan/spyder/config"
 	"github.com/aderjaan/spyder/db"
-	"github.com/rwynn/gtm"
-	"labix.org/v2/mgo"
 )
 
 func main() {
@@ -17,41 +15,7 @@ func main() {
 	session := db.GetSession(config.MongoHost)
 	defer session.Close()
 
-	readOplog(session, config)
+	db.ReadOplog(session, config)
 
 	log.Println("exiting spyder...")
-}
-
-func getFilter(op *gtm.Op) bool {
-	//TODO work out filtering
-	return op.Operation == "u" &&
-		op.GetDatabase() == "safetyapps" &&
-		op.GetCollection() == "shared.apps"
-}
-
-func readOplog(session *mgo.Session, config config.Conf) {
-	var err error
-
-	ops, errs := gtm.Tail(session, &gtm.Options{nil, getFilter})
-	// Tail returns 2 channels - one for events and one for errors
-	for {
-		// loop forever receiving events
-		select {
-		case err = <-errs:
-			// handle errors
-			log.Println(err)
-		case op := <-ops:
-			// op will be an insert, delete or update to mongo
-			// you can check which by calling op.IsInsert(), op.IsDelete(), or op.IsUpdate()
-			// op.Data will get you the full document for inserts and updates
-			log.Printf(`Got op <%v> for object <%v>
-			   in database <%v>
-			   and collection <%v>
-			   and data <%v>
-			   and timestamp <%v>`,
-				op.Operation, op.Id, op.GetDatabase(),
-				op.GetCollection(), op.Data, op.Timestamp)
-			//log.Println(msg) // or do something more interesting
-		}
-	}
 }
