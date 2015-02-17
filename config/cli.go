74f4857413f7ee7da19504a988aff5ff67e673ac
Piyush Verma <github@piyushverma.net>
2015-02-17 14:45:36 +0530

    Move cli to a separate file

---
+++ config/cli.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"flag"
+	"log"
+)
+
+func cliArgs() string {
+	var config = flag.String(
+		"config",
+		"",
+		"Config [.yml format] file to load the configurations from",
+	)
+
+	flag.Parse()
+
+	if *config == "" {
+		log.Println("No config file supplied. Using defauls.")
+	}
+
+	return *config
+}
