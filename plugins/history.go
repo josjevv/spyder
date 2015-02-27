900720eff287befb80cc3637dd0c527b76aa507a
aderjaan <aderjaan.vandenberg@changer.nl>
2015-02-27 16:13:38 +0100

    some refactoring, utils dir

--- plugins/history.go
+++ plugins/history.go
@@ -2,16 +2,16 @@ package plugins
 
 import (
 	"log"
-	//"time"
 
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 
 	config "github.com/changer/spyder/config"
 	db "github.com/changer/spyder/db"
-	bson "gopkg.in/mgo.v2/bson"
+	utils "github.com/changer/spyder/utils"
 )
 
-var BLACKLIST []string = []string{"__v", "date_created", "date_updated", "update_spec"}
+var _BLACKLIST []string = []string{"__v", "date_created", "date_updated", "update_spec"}
 
 func HistoryListener(settings *config.Conf, session *mgo.Session) chan *db.Fly {
 	channel := make(chan *db.Fly)
@@ -40,7 +40,7 @@ func historyHandler(settings *config.Conf, session *mgo.Session, fly *db.Fly) {
 	if set != nil {
 		var setMap = set.(bson.M)
 		for key, value := range setMap {
-			if !stringInSlice(key, BLACKLIST) {
+			if !utils.StringInSlice(key, _BLACKLIST) {
 				log.Printf("fly.Object['$set'][%v] = %v", key, value)
 				hist := createNewHistory(fly, key, value.(string))
 				hist.From = getHistoricValue(session, settings.MongoDb, fly.Id, key)
@@ -75,50 +75,6 @@ func insertHistory(session *mgo.Session, dbName string, hist *history) {
 	if err != nil {
 		log.Println("Insert for history failed : " + err.Error())
 	}
-}
-
-func getHistories(session *mgo.Session, dbName string, id string, collection string) ([]history, bool) {
-	var result []history
-	c := session.DB(dbName).C("shared.history")
-
-	log.Printf("Id: %v , collection: %v", id, collection)
-	err := c.Find(bson.M{"entity.$id": id, "entity.$ref": collection}).Sort("-date").All(&result)
-
-	if err != nil {
-		log.Print("History for key not found : " + err.Error())
-		return result, false
-	}
-	return result, true
-}
-
-// func getOldValue() {
-// 	if !fly.IsInsert() {
-// 		histories, found := getHistories(session, settings.MongoDb, fly.Id, fly.GetCollection())
-// 		if found && len(histories) > 0 {
-// 			var entry = histories[0]
-// 			log.Printf("Hist0.%v: %v", "Date", entry.Date)
-// 			log.Printf("Hist0.%v: %v", "DateCreated", entry.DateCreated)
-// 			log.Printf("Hist0.%v: %v", "DateUpdated", entry.DateUpdated)
-
-// 			//Entity won't get loaded
-// 			log.Printf("Hist0.%v: %v", "Entity.Id", entry.Entity.Id)
-// 			log.Printf("Hist0.%v: %v", "Entity.Ref", entry.Entity.Ref)
-
-// 			//id won't get loaded
-// 			log.Printf("Hist0.%v: %v", "Id", entry.Id.Hex())
-// 			log.Printf("Hist0.%v: %v", "Organization", entry.Organization.Hex())
-// 			log.Printf("Hist0.%v: %v", "User", entry.User.Hex())
-// 		}
-// 	}
-// }
-
-func stringInSlice(a string, list []string) bool {
-	for _, b := range list {
-		if b == a {
-			return true
-		}
-	}
-	return false
 }
 
 func createNewHistory(fly *db.Fly, key string, value string) history {
